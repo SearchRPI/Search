33a19f770e6f4d5b608099add06360e05b6f7e86
philippark <philippark271@gmail.com>
2025-01-31 17:23:52 -0500

    bm25 ranking algorithm implemented

--- include/weight.h
+++ include/weight.h
@@ -1,13 +1,32 @@
 #pragma once
 
+#include <unordered_map>
+#include <string>
+#include <math>
+
 /**
  * @file  weight.h
  * @brief Weighting schemes
 */
 
 namespace SearchRPI {
+class BM25 {
+    public:
+        BM25(const std::unordered_map<std::string, int>& doc_freqs, int total_docs, double k1 = 1.5, double b = 0.75);
 
-// TODO: Ranking Algorithm Here (BM25)
-//       Use class so swap to BM25f (Incorporating Field Weights) will be easier
-
+        /**
+         * @brief Compute BM25 score for a document
+         * @param term_freqs Map of term frequencies in the document
+         * @param doc_length Length of the document
+         * @param avg_doc_length Average document length in corpus
+         * @return BM25 score
+        */
+        double computeScore(const std::unordered_map<std::string, int>& term_freqs, int doc_length, double avg_doc_length);
+    
+    private:
+        int N; //total number of documents
+        double k1, b;
+        std::unordered_map<std::string, double> idf; //stores precomputed idf for documents
+        void computeIDF(const std::unordered_map<std::string, int>& doc_freqs); 
+};
 }
--- src/search/weight.cc
+++ src/search/weight.cc
@@ -1,1 +1,36 @@
 #include "weight.h"
+
+namespace SearchRPI {
+
+BM25::BM25(const std::unordered_map<std::string, int>& doc_freqs, int total_docs, double k1 = 1.5, double b = 0.75) {
+    N = total_docs;
+    k1 = k1;
+    b = b;
+
+    computeIDF(doc_freqs);
+}
+
+double BM25::computeScore(const std::unordered_map<std::string, int>& term_freqs, int doc_length, double avg_doc_length) {
+    double score = 0.0;
+
+    for (const auto& [term, df] : term_freqs){
+        if (idf[term] == idf.end()){
+            continue;
+        }
+
+        double tf_component = (freq * (k1 + 1.0)) / (freq + k1 * (1 - b + b * (doc_length / avg_doc_length)));
+
+        score += idf[term] * tf_component;
+    }
+
+    return score;
+}
+
+
+void BM25::computeIDF(const std::unordered_map<std::string, int>& doc_freqs) {
+    for (const auto& [term, df] : doc_freqs){
+        idf[term] = std::log((N - df + 0.5)/(df + 0.5) + 1.0);
+    }
+}
+
+}
